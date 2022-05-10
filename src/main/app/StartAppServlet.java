92f13d91a7eb18acd4841a1746ac517e3a0e583b
Takamine Yusuke <22takamine_yuusuke@axiz.co.jp>
2022-05-10 15:26:15 +0900

    移行

--- src/main/app/StartAppServlet.java
+++ src/main/app/StartAppServlet.java
@@ -2,6 +2,7 @@ package app;
 
 import java.io.IOException;
 
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -11,7 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class StartAppServlet
  */
-@WebServlet("/StartAppServlet")
+@WebServlet({"/LoginServlet","/login"})
 public class StartAppServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
@@ -35,9 +36,52 @@ public class StartAppServlet extends HttpServlet {
     /**
      * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
      */
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		request.setCharacterEncoding("UTF-8");
+	    String name = request.getParameter("name");
+	    String pass = request.getParameter("pass");
 
-    	// ここに必要な処理を実装してください。
-    }
+	    String msg = "IDまたはPASSが間違っています";
+	    String nameMsg = "IDは必須です";
+	    String passMsg = "PASSは必須です";
+	    String path = "/login.jsp";
+	    int frag[] = {0,0};
+	
+	    if (!Utility.isNullOrEmpty(name)) {
+	    	frag[0] = 1;
+	    	
+	    }else {
+	    	request.setAttribute("name", nameMsg);
+	    	
+	    }
+	    
+	    if (!Utility.isNullOrEmpty(pass)) {
+	    	frag[1] = 1;
+	    	
+	    }else {
+	    	request.setAttribute("pass", passMsg);
+	    	
+	    }
+	    
+	    if(frag[0] == 1 && frag[1] == 1) {
+	    	if(name.equals("axiz") && pass.equals("axizuser")) {
+	    		msg = "ようこそ、" + name + "さん";
+	    		path = "/result.jsp";
+	    		request.setAttribute("result", msg);
+	    		//request.getRequestDispatcher(path).forward(request, response);
+	    		
+	    	}else {
+	    		request.setAttribute("msg", msg);
+	    		
+	    	}
+	    	
+	    
+	    
+	    }
+		request.getRequestDispatcher(path).forward(request, response);	
+	    
+	}
+
+
 }
